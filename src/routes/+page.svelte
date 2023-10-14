c96b4a211f7dc00ab0097cd589be6d3cc01b4bcb
juliano <julianoh1909@gmail.com>
2023-10-14 15:41:12 -0300

    primeiro codigo da bianca

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let core = ''
+
+	const inserircore = ()=>{
+		core='❤'
+	}
+
+
+	const tirarcore = ()=>{
+		core=''
+	}
+
+
+
+
+</script>
+<button class= 'w-12 h-6 m-3 rounded-md text-white bg-red-700' on:click={inserircore} >
+	click 		
+</button>
+<button class= 'w-12 h-6 m-3 rounded-md text-white bg-red-700' on:click={tirarcore}>
+	click 2 		
+</button>
+<div class='w-36 h-36 bg-blue-600 flex justify-center items-center'>
+	<div class='w-24 h-24 bg-gray-200 flex justify-center items-center animate'>
+		<div class= 'w-12 h-12 bg-pink-500 flex justify-center items-center animate'>
+			<div   class='w-8 h-8 animate text-white bg-black flex justify-center items-center' >
+				{core}
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
